9d4aaad25411f31a40198c7c776f8360341d3f32
MadMcCrow <noe.perard+git@gmail.com>
2026-09-25 22:36:21 +0200

    change linux builder config

--- packages/linux-builder.nix
+++ packages/linux-builder.nix
@@ -1,16 +1,24 @@
 {
   pkgs,
   lib,
+  lix,
   darwin,
   ...
 }:
 darwin.linux-builder.override {
   modules = [
     {
+      nix = {
+        package = lix;
+        settings.experimental-features = ["nix-command" "flakes"];
+        # settings.auto-optimise-store = true;
+      };
+      # expose x86_64-linux
+      boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
       # force Apple's vGIC
       virtualisation.qemu.options = [
         "-machine"
-        "virt,gic-version=host"
+        "virt"
       ];
     }
   ];
