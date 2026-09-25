c68a07f83ab7699b4903e39dd27891c8bc4c5504
MadMcCrow <noe.perard+git@gmail.com>
2026-09-25 14:25:37 +0200

    WIP github integration

---
+++ packages/darwin-update.nix
@@ -0,0 +1,25 @@
+# auto updater tool.
+# TODO :
+# - use a just recipe for better scripting and fzf TUI
+# - detect system vs hostname in --flake
+# - use npins for auto version bump
+{
+  lib,
+  writeShellApplication,
+  system,
+  nix,
+  just,
+  npins,
+  darwin,
+  ...
+}:
+let
+  repo = "github:MadMcCrow/darwin-config";
+in
+writeShellApplication {
+          name = "darwin-update";
+          runtimeInputs = [ nix just];
+          text = ''
+              sudo darwin-rebuild --flake ${repo}#${system} switch
+          '';
+        };
